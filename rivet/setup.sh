8c7b82f711376aef836578bae17cac79959d06e5
Ben Wilson <benjamin.james.wilson@cern.ch>
2023-01-24 11:26:47 +0100

    fix rivet setup

--- rivet/setup.sh
+++
@@ -1,4 +0,0 @@
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-asetup AthGeneration,21.6.36
-source setupRivet.sh
--- rivet/setup_rivet.sh
+++ rivet/setup_rivet.sh
@@ -1,3 +1,3 @@
 setupATLAS
-asetup 21.6.99,AthGeneration
-source ${LCG_RELEASE_BASE}/LCG_88/MCGenerators/rivet/${RIVETVER}/${LCG_PLATFORM}/rivetenv.sh
+asetup 21.6.1, AthGeneration
+source ${LCG_RELEASE_BASE}/LCG_88/MCGenerators/rivet/3.1.2/${LCG_PLATFORM}/rivetenv.sh
