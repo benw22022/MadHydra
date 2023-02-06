2687d0e7566de5b854981ce97b5787f97d0aa766
Ben Wilson <benjamin.james.wilson@cern.ch>
2023-02-06 10:57:39 +0100

    compiler script

--- tests/data/2023-01-24_13-39-16_typeII_single_prod_WWjj_100_1.0/compile_rivet_routine_pp_dppjj_llvvjj.sh
+++ tests/data/2023-01-24_13-39-16_typeII_single_prod_WWjj_100_1.0/compile_rivet_routine_pp_dppjj_llvvjj.sh
@@ -1,6 +1,6 @@
-echo hello
-setupATLAS
-lsetup root
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+lsetup "views LCG_102b_ATLAS_2 x86_64-centos7-gcc11-opt"
 asetup 21.6.1, AthGeneration
 source ${LCG_RELEASE_BASE}/LCG_88/MCGenerators/rivet/3.1.2/${LCG_PLATFORM}/rivetenv.sh
-rivet-build /afs/cern.ch/work/b/bewilson/doubly_charged_higgs_generation/TypeII_joboptions/rivet/routines/pp_dppjj_llvvjj.cc
+rivet-build -r /afs/cern.ch/work/b/bewilson/doubly_charged_higgs_generation/TypeII_joboptions/rivet/routines/pp_dppjj_llvvjj.cc |& tee compile.log
