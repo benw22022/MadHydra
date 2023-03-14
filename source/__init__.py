654578687d066a463c7a4d6fcef744b0ceb495ba
Benjamin Wilson <bewilson@login01.af.uchicago.edu>
2023-03-14 07:06:22 -0500

    updated file

--- source/__init__.py
+++ source/__init__.py
@@ -1,3 +1,3 @@
-from .utils import launch_process, delete_files_with_extn
+from .utils import launch_process, delete_files_with_extn, get_files_with_extn
 from .condor import submit_job
 from .generate import write_proc_card, run_cleanup, run_local_generation
